6d4a5372267c1ea78e76ca818f96bbb5d44a52dc
Huska, Matthew <HuskaM@rki.de>
2022-06-30 14:57:24 +0200

    Add simple test that the --help argument returns a 0 exit code

--- tests/test_sonar.py
+++ tests/test_sonar.py
@@ -3,6 +3,14 @@ import pytest
 from covsonar import sonar
 
 
+def test_help():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        parsed_args = sonar.parse_args(['--help'])
+        sonar.main(parsed_args)
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
 def test_setup_db(tmp_path):
     parsed_args = sonar.parse_args(['setup', '--db', str(tmp_path / 'test.db')])
     retval = sonar.main(parsed_args)
