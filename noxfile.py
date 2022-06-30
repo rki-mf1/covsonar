5e8ed226bec636489da7f577de21b64ce9cf4ae8
Huska, Matthew <HuskaM@rki.de>
2022-06-30 15:03:50 +0200

    Fix flake8 message about noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,7 @@ from nox.sessions import Session
 
 package = "covsonar"
 nox.options.sessions = "lint", "tests"
-locations = "src", "tests", "noxfile.py"
+locations = "src", "tests", "./noxfile.py"
 
 
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
